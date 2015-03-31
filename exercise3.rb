6017d9849f04be6f83a17604196c855a68e8a09f
Jordan <jordanmcrae@hotmail.com>
2015-03-31 13:57:26 -0400

    Added birth-year calculation.

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,10 @@
 puts "What's your name?"
 name = gets.chomp
 puts "Hi, #{name}!"
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+year = 2015 - age
+
+puts "If you are #{age}, then you were born in the year #{year}."
