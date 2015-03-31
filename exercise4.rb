465f300576ea1b0576bb562b0caeeb3d60a1b3c6
Jordan <jordanmcrae@hotmail.com>
2015-03-31 15:54:30 -0400

    Reorganized program

--- exercise4.rb
+++ exercise4.rb
@@ -3,10 +3,10 @@
 (1..100).each do |x|
 	if x % 5 == 0 && x % 3 == 0
 		puts "BitMaker"
-	elsif x % 3 == 0
-		puts "Bit"
 	elsif x % 5 == 0
 		puts "Maker"
+	elsif x % 3 == 0
+		puts "Bit"
 	else
 		puts x
 	end
