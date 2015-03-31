7a65939c3d77dc73354aa2d32b56dbc2ee89dbc2
Jordan <jordanmcrae@hotmail.com>
2015-03-31 17:33:51 -0400

    Updated program to utilize the Time instance rather than a fixed year.

--- exercise3.rb
+++ exercise3.rb
@@ -2,9 +2,12 @@ puts "What's your name?"
 name = gets.chomp
 puts "Hi, #{name}!"
 
+current_year = Time.new
+
 puts "How old are you?"
 age = gets.chomp.to_i
 
-year = 2015 - age
+birthday_already = (current_year.year - age)
+upcoming_birthday = (current_year.year - age - 1)
 
-puts "If you are #{age}, then you were born in the year #{year}."
+puts "If you are #{age}, you were born in either the year #{birthday_already} or in the year #{upcoming_birthday}."
